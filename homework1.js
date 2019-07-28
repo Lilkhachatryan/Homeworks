6e29f929ffa0511105610ebd8e23daad335fb196
Lilkhachatryan <lilit-messi@mail.ru>
2019-07-28 22:30:50 +0400

    syntax fixes, homework1

--- homework1.js
+++ homework1.js
@@ -1,65 +1,77 @@
+    
 // 1. Write a function which receives an array and a number as arguments and returns a new array
 // from the elements of the given array which are larger than the given number.
 
-function filterArray(arr = [], n) {
-	if(!n) {
-		return "Enter number";
-	}
+const filterArray = (arr = [], n = Number.POSITIVE_INFINITY) => {
 
-	let filtered = [];
+	if( !Number.isInteger(n) ) return "Enter number";
 
-	// shorter method
-	// filtered = arr.filter( item => item > n)
+	let filtered;
 
-	for(let i=0; i<arr.length; i++) {
-		if(arr[i] > n) {
-			filtered.push(arr[i]);
-		}
-	}
+	/**
+	 * Time ~ 0.176ms
+	 */
+	filtered = arr.filter( item => item > n);
 
-	return filtered.length !== 0 ? filtered : "Such values don't exist";
-}
+	/**
+	 * Time ~ 0.184ms
+	 */
+	// for( let i = 0; i < arr.length; i++ ) {
+	// 	if( arr[i] > n ) {
+	// 		filtered.push(arr[i]);
+	// 	}
+	// }
 
-const filteredArray = filterArray([10, 25, 16, -5, 4, 48, 78], 16);
-console.log(filteredArray);
+	return !!filtered.length ? filtered : "Such values don't exist";
+};
+
+const filteredArray = filterArray([10, 25, 16, -5, 4, 48, 78]);
+//console.log(filteredArray);
+
 
 
 // 2. Write a function, which receives two numbers as arguments and finds all numbers between
 // them such that each digit of the number is even. The numbers obtained should be printed in a
 // comma-separated sequence on a single line.
 
-function findEven(start = 0, end = 0) {
-	if(start>end) {
-		return "First argument can't be greater"
-	}
+const findEven(start = 0, end = 0) => {
+
+	if( start > end ) return "First argument can't be greater";
 
 	let evenNumbers = [];
 
-	for(let i = start; i <= end; i++) {
-		let current = [...i.toString()];
-		let isEven = true;
+	for( let i = start; i <= end; i++ ) {
+		// let current = [...i.toString()];
+		//let isEven = true;
 
-		for(let j =0; j<current.length; j++) {
-			if(current[j] % 2!== 0){
-				isEven = false;
-			} 
-		}
+		/**
+		 * Time 2.7
+		 */
+		// let format = /[13579]/;
+		let isValid = /[13579]/.test(i.toString());
 
-		if(isEven) {
-			evenNumbers.push(i);
-		}
+		/**
+		 * Time 11.2
+		 */
+		// for( let j =0; j < current.length; j++ ) {
+		// 	if( current[j]%2 !== 0){
+		// 		isEven = false;
+		//    // Missed break!!!!
+		// 		break;
+		// 	}
+		// }
+
+		if(!isValid) evenNumbers.push(i);
 	}
+
 	return evenNumbers.join(', ');
 }
 
-const findedEven = findEven(19,42);
-console.log(findedEven);
-
-
+const findedEven = findEven(10000,21000);
 
 // 3. Write a recursive function to determine whether all digits of the number are odd or not.
 
-function isOddDigitsOfNumber(number) {
+const isOddDigitsOfNumber(number) => {
 	if(!number) {
 		return "Enter argument"
 	}
@@ -86,7 +98,7 @@ console.log(res)
 // 4. Given an array of numbers. Write a recursive function to find its minimal positive element. (if such
 // element does not exist, return -1)․
 
-function minOfArray(arr = [], n = Infinity) {
+const minOfArray(arr = [], n = Infinity) => {
 	if(arr.length === 0) {
 		return n !== Infinity ? n : -1
 	}
@@ -97,16 +109,16 @@ function minOfArray(arr = [], n = Infinity) {
 }
 
 const minElem = minOfArray([8,7,-3]);
-console.log(minElem);
+// console.log(minElem);
 
 
 
 
-//  5. Given an array of numbers which is almost sorted in ascending order.  Find the index where sorting
+//  5. Given an array of numbers which is almost sorted in ascending order.  Find the index where sorting
 //  order is violated.
 
-function findViolatedIndex(arr = []) {
-	for(let i = 0; i < arr.length -1 ; i++) {
+const findViolatedIndex(arr = []) => {
+	for(let i = 0; i < arr.length - 1 ; i++) {
 		if(arr[i] > arr[i + 1]) {
 			return i+1;
 		}
@@ -115,4 +127,4 @@ function findViolatedIndex(arr = []) {
 }
 
 let violatedIndex = findViolatedIndex([2, 12, 25, 5, 48]);
-console.log(violatedIndex);
+// console.log(violatedIndex);
